efbd30d8bbcfb96855729cdb342778e2e6a83774
Paul Schifferer <paul@schifferers.net>
2023-01-02 11:33:29 -0800

    Initial commit

---
+++ src/wsgi.py
@@ -0,0 +1,6 @@
+__author__ = "Paul Schifferer <paul@schifferers.net>"
+"""
+"""
+
+from statesman_slack.main import create_app
+app = create_app()
---
+++ tests/test_this.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <paul@schifferers.net>"
+"""
+Test stub.
+"""
+
+
+def test_of_this():
+    this = True
+    assert this
